bca8ecf7cac123ef4846a81417de7b7076efb35d
Amaliki Youness <youness.amaliki@student.ap.be>
2018-04-16 15:46:23 +0200

    Final
    Onafgewerkt, moet nog Redis gedeelte afwerken

--- SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/RedisApplication.java
+++ SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/RedisApplication.java
@@ -9,39 +9,57 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import edu.ap.spring.model.InhaalExamen;
-import edu.ap.spring.model.InhaalExamenRepository;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import edu.ap.spring.controller.RedisController;
 import edu.ap.spring.redis.RedisService;
-
+import edu.ap.spring.model.InhaalExamen;
 
 @SpringBootApplication
 public class RedisApplication {
+	
+	private RedisService service;
+	private String CHANNEL = "edu:ap:redis";
+	private String KEY = "edu:ap:test";
+	
+	@Autowired
+	public void setRedisService(RedisService service) {
+		this.service = service;
+	}
+	
+	@Bean
+	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+											MessageListenerAdapter listenerAdapter) {
 
-	@Autowired
-	private InhaalExamen inhaalexamen;
-	
-	@Autowired
-	private InhaalExamenRepository repository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(RedisApplication.class, args);
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new ChannelTopic(CHANNEL));
+
+		return container;
+	}
+
+	@Bean
+	MessageListenerAdapter listenerAdapter(RedisController controller) {
+		return new MessageListenerAdapter(controller, "onMessage");
 	}
 	
 	@Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return (args) -> {
-	 		InhaalExamen inhaalexamen1 = new InhaalExamen("Jones", "Webtech", "Ziek");
-	 		repository.save(inhaalexamen1);
 	 		
+	 		// Movies;
+			InhaalExamen inhaalexamen1 = new InhaalExamen("Jones", "Webtech", "Ziek");
+	 		Map<String, String> examens = new HashMap<String, String>();
+	 		examens.put("examen1", inhaalexamen1.toString());
+	 		examens.clear();
+	 
 	 		
 		};
     }
-
-    
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
 }
--- SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/controller/RedisController.java
+++ SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/controller/RedisController.java
@@ -1,5 +1,61 @@
 package edu.ap.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import edu.ap.spring.redis.RedisService;
+
+@Controller
 public class RedisController {
 
+   private List<String> redisMessages = new ArrayList<String>();
+   private RedisService service;
+	
+   @Autowired
+	public void setRedisService(RedisService service) {
+		this.service = service;
+   }
+     
+   
+   @RequestMapping("/list")
+   @ResponseBody
+   public String list() {
+
+	   String html = "<HTML>";
+	   // get the bitcount of our counter
+	   html += "<BODY><h1>" + service.bitCount("examenscount") + " Examens</h1><br/><br/><ul>";
+	   
+	   Set<String> examens = service.keys("examens:*");
+	   for(String e : examens) {
+		   // make a key from the byte array
+		   String key = new String(e);
+		   Map<Object, Object> reasons = service.hgetAll(key);
+		   String[] parts = key.split(":");
+		   
+		   html += "<li>" + parts[2] + " (" + parts[1] + ")<br/>";
+		   html += "Examens : ";
+		   // iterate over actors
+		   for(Entry<Object, Object> entry : reasons.entrySet()) {
+			   html += entry.getValue() + ", ";
+		   }
+		   // strip off last ', '
+		   html = html.substring(0, html.length() - 2);
+	   }
+	   html += "</BODY></HTML>";
+	   
+	   return html;
+   }
+   
+   // Messaging
+   public void onMessage(String message) {
+	   this.redisMessages.add(message);
+   }
 }
